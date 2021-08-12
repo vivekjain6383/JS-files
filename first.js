a4a54d34e20d23978b15031f60cc8d117e68e2c0
vivekjain6383 <66308030+vivekjain6383@users.noreply.github.com>
2021-08-12 14:23:22 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,45 @@
+
+// window.onload = function load(){
+//     alert("the window is loaded");
+// }
+// let prev = document.querySelectorAll('.class1')[2].innerHTML;
+// clickna.addEventListener('mousedown', function(){
+//     document.querySelectorAll('.class1')[2].innerHTML="<b>The button is clicked!!</b>"
+//     console.log("clicked!!");
+// })
+// clickna.addEventListener('mouseover', function(){
+//     console.log("Mouse overed!!");
+// })
+// clickna.addEventListener('mouseout', function(){
+//     console.log("Mouse out!!");
+//})
+// c = document.querySelectorAll('#clickna');
+// c[0].addEventListener('mouseover', function(){
+//     console.log("Mouse overed!!");
+// })
+// c[0].addEventListener('mouseout', function(){
+//     console.log("Mouse out!!");
+// })
+// c = document.querySelectorAll('#clickna');
+// c[0].addEventListener('mouseup', function(){
+//     document.querySelectorAll('.class1')[2].innerHTML=prev;
+//     console.log("Mouse up!!");
+// })
+// c[0].addEventListener('mousedown', function(){
+// //     console.log("Mouse down!!");
+// // })
+// habu = ()=>{
+//     document.querySelectorAll('.class1')[1].innerHTML="habu";
+// }
+// Jhhabu = ()=>{
+//     document.querySelectorAll('.class1')[1].innerHTML="Jhhabu";
+// }
+// setTimeout(habu, 1000);
+// setTimeout(Jhhabu,2000);
+// setInterval(habu,2000);
+// setInterval(Jhhabu,2000);
+// obj = {name: "Vivek", age: 22};
+// jso = JSON.stringify(obj);
+// console.log(jso);
+// a = 45;
+// console.log(`The value of a is ${a}`);
---
+++ variables.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+console.log("VJ's JS");
+console.log(5**3);
+var n=82;
+var m=36;
+console.log(m+n);
+
